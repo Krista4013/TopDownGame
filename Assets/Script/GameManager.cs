d257bb51a70e478cf0570a69736f6965092fca01
Krista4013 <jm761212@gmail.com>
2024-05-14 07:34:18 +0900

    [Feat] 기능추가
    이름 입력 시스템
    캐릭터 선택 시스템
    시간 표시 시스템
    인게임 이름 바꾸기 시스템
    인게임 캐릭터 변경 시스템
    
    구현

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,20 +3,141 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private Text TimeTxt;
+    [SerializeField]
+    private GameObject StartUI;
+
+    [SerializeField]
+    private GameObject Select;
+    [SerializeField]
+    private GameObject GreenLizard;
+    [SerializeField]
+    private GameObject BlueLizard;
+    [SerializeField]
+    private GameObject CharacterSelect;
+    [SerializeField]
+    private Text PlayerName;
+    [SerializeField]
+    private GameObject InJoin;
+    [SerializeField]
+    private InputField inputField;
+    [SerializeField]
+    private InputField ChangeinputField;
+    [SerializeField]
+    private GameObject MainSprite1;
+    [SerializeField]
+    private GameObject MainSprite2;
+    [SerializeField]
+    private GameObject ChangeName;
+    [SerializeField]
+    private GameObject ChangeNameTxt;
+    [SerializeField]
+    private GameObject ChangeSelectBtn;
+    [SerializeField]
+    private GameObject ChangeUI;
+    [SerializeField]
+    private GameObject changeCharacterUI;
+
+    public bool SelectG = false;
+    public bool SelectB = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;
+        StartUI.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         TimeTxt.text = DateTime.Now.ToString("HH : mm");
+    }
+
+    public void SelectBtn()
+    {
+        GreenLizard.SetActive(false);
+        BlueLizard.SetActive(false);
+        CharacterSelect.SetActive(true);
+    }
+
+    public void selectGreen()
+    {
+        SelectB = false;
+        SelectG = true;
+        CharacterSelect.SetActive(false);
+        GreenLizard.SetActive(true);
+    }
+
+    public void selectBlue()
+    {
+        SelectB = true;
+        SelectG = false;
+        CharacterSelect.SetActive(false);
+        BlueLizard.SetActive(true);
+    }
+    
+    public void LoginBtn()
+    {
+        if (inputField.text.Length > 1 && inputField.text.Length < 11)
+        {
+
+            if ((SelectG == true) && (SelectB == false))
+            {
+                MainSprite1.SetActive(true);
+            }
+
+            else if ((SelectG == false) && (SelectB == true))
+            {
+                MainSprite2.SetActive(true);
+            }
+
+            PlayerName.text = inputField.text;
+            StartUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void ChangeNameUI()
+    {
+        ChangeUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ChangeBtn()
+    {
+        if (inputField.text.Length > 1 && inputField.text.Length < 11)
+        {
+            PlayerName.text = ChangeinputField.text;
+            ChangeUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void ChangeCharacterUI()
+    {
+        changeCharacterUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ChangeGreen()
+    {
+        MainSprite2.SetActive(false);
+        MainSprite1.SetActive(true);
+        changeCharacterUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ChangeBlue()
+    {
+        MainSprite1.SetActive(false);
+        MainSprite2.SetActive(true);
+        changeCharacterUI.SetActive(false);
+        Time.timeScale = 1;
     }
 }
---
+++ Assets/Script/TopDownAimRotation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer armRenderer;
+    [SerializeField]
+    private Transform armPivot;
+    [SerializeField]
+    private SpriteRenderer characterRenderer1;
+    [SerializeField]
+    private SpriteRenderer characterRenderer2;
+
+    private TopDownController controller;
+
+
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        RotateArm(direction);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer1.flipX = Mathf.Abs(rotZ) > 90f;
+        characterRenderer2.flipX = Mathf.Abs(rotZ) > 90f;
+
+
+        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
