3e9f25eee7303791dedb56068359ee1d0a93ad77
Krista4013 <jm761212@gmail.com>
2024-05-13 19:49:45 +0900

    [Move]

---
+++ Assets/Script/PlayerInputController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveinput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveinput);
+
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.WorldToScreenPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
